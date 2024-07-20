2a49621d8be7513412ea8fd3d7266aecf307657c
Jeewapadma <127758408+IT22274120@users.noreply.github.com>
2024-07-20 12:25:20 +0530

    update header component and make profile private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,26 +1,37 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+  
   return (
     <div className='bg-slate-200'>
-        <div className='flex justify-between items-center max-w-5xl mx-auto p-3'>
-            <Link to='/'>
-            <h1 className='font-bold'>Auth App</h1>
-            </Link>
-            <ul className='flex gap-6 '>
-                <Link to='/'>
-                <li>Home</li>
-                </Link>
-
-                <Link to='/about'>
-                <li>About</li>
-                </Link>
-
-                <Link to='/sign-in'>
-                <li>SignIn</li>
-                </Link>
-            </ul>
-        </div>
+      <div className='flex justify-between items-center max-w-5xl mx-auto p-3'>
+        <Link to='/'>
+          <h1 className='font-bold'>Auth App</h1>
+        </Link>
+        <ul className='flex gap-6'>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            {currentUser ? (
+              <Link to='/profile'>
+                <img
+                  src={currentUser.profilePicture}
+                  alt="profile"
+                  className="h-7 w-7 rounded-full object-cover"
+                />
+              </Link>
+            ) : (
+              <Link to='/sign-in'>SignIn</Link>
+            )}
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
